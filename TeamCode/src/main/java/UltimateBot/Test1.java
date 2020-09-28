c05d8cb7eef8050aac15cc24c1bb9b0a675ed966
chickenados <chickenadosrobotics@gmail.com>
2020-09-27 18:12:51 -0700

    first program test commit/push

---
+++ TeamCode/src/main/java/UltimateBot/Test1.java
@@ -0,0 +1,19 @@
+package UltimateBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Test1 extends OpMode {
+
+    @Override
+    public void init() {
+        String name = "Claire";
+        telemetry.addData("Hello", name );
+    }
+    @Override
+    public void loop() {
+
+    }
+
+}
