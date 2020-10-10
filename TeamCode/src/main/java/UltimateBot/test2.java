b24347d2179903575202a2cdc141755c3d434b25
saharrington <simon.a.harrington@gmail.com>
2020-10-09 19:53:11 -0700

    bONk

--- TeamCode/src/main/java/UltimateBot/test2.java
+++ TeamCode/src/main/java/UltimateBot/test2.java
@@ -1,12 +1,20 @@
 package UltimateBot;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import java.lang.annotation.Annotation;
 
 @Autonomous (name = "test2")
 
-public class test2 extends Autonomous {
+public class test2 extends LinearOpMode {
 
-    @Override
+    @Override public void main() throws InterruptedException {
+
+
+
+    }
+
+
 }
