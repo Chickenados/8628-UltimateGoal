7bacdd7c10400cbc64dc574ed28d7e51a8d66ba0
saharrington <simon.a.harrington@gmail.com>
2020-10-09 19:17:36 -0700

    this

---
+++ TeamCode/src/main/java/UltimateBot/test2.java
@@ -0,0 +1,12 @@
+package UltimateBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.lang.annotation.Annotation;
+
+@Autonomous (name = "test2")
+
+public class test2 extends Autonomous {
+
+    @Override
+}
