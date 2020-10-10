5570f6dfdc7117cab52f36e5c332dd75a470f330
saharrington <simon.a.harrington@gmail.com>
2020-10-10 13:16:44 -0700

    commiting color sensor program - moves around a lot.

--- TeamCode/src/main/java/UltimateBot/test2.java
+++ TeamCode/src/main/java/UltimateBot/test2.java
@@ -3,16 +3,76 @@ package UltimateBot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.lang.annotation.Annotation;
 
 @Autonomous (name = "test2")
 
 public class test2 extends LinearOpMode {
-    
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+    ColorSensor colorSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+
+          colorSensor = hardwareMap.colorSensor.get("colorSensor");
+
+
+        leftFront = hardwareMap.dcMotor.get("frontLeft");
+        rightFront = hardwareMap.dcMotor.get("frontRight");
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack = hardwareMap.dcMotor.get("backLeft");
+        rightBack = hardwareMap.dcMotor.get("backRight");
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        colorSensor.enableLed(true);
+        waitForStart();
+        int x = 0;
+        int colorSensed = colorSensor.argb();
+        while (x<1) {
+            if (colorSensed >= 330 && colorSensed <= 360) {
+                String color = "red";
+                leftFront.setPower(1);
+                rightFront.setPower(1);
+                rightBack.setPower(1);
+                leftBack.setPower(1);
+                sleep(300);
+                leftFront.setPower(0);
+                rightFront.setPower(0);
+                rightBack.setPower(0);
+                leftBack.setPower(0);
+                x = 3;
+                return;
+            }
+        }
+
+        leftFront.setPower(1);
+        rightFront.setPower(1);
+        leftBack.setPower(1);
+        rightBack.setPower(1);
+        sleep(500);
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        sleep(500);
+
+
+        leftFront.setPower(1);
+        rightFront.setPower(-1);
+        leftBack.setPower(1);
+        rightBack.setPower(-1);
+        sleep(500);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
     }
 }
+
