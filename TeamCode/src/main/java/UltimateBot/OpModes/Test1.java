053504b345e86de4a6153ae9f0f3f1f67b0d0e96
saharrington <simon.a.harrington@gmail.com>
2020-10-11 16:44:14 -0700

    removed weird stuff in test1

--- TeamCode/src/main/java/UltimateBot/OpModes/Test1.java
+++ TeamCode/src/main/java/UltimateBot/OpModes/Test1.java
@@ -8,11 +8,8 @@ public class Test1 extends OpMode {
 
     @Override
     public void init() {
-<<<<<<< HEAD:TeamCode/src/main/java/UltimateBot/Test1.java
-        String name = "Simon";
-=======
         String name = "Maggie";
->>>>>>> 767fc7ecc46348e7d7fc670c0a18f123a700d9ef:TeamCode/src/main/java/UltimateBot/OpModes/Test1.java
+
         telemetry.addData("Hello", name );
     }
     @Override
