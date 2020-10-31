21b353393fb3ee8916d4b43856f49a2909e3f28d
saharrington <simon.a.harrington@gmail.com>
2020-10-30 19:15:03 -0700

    Added the option for a second color sensor facing top down.

--- TeamCode/src/main/java/UltimateBot/REVColorSensor.java
+++ TeamCode/src/main/java/UltimateBot/REVColorSensor.java
@@ -32,20 +32,21 @@ public class REVColorSensor extends LinearOpMode {
      * It has a light/distance (range) sensor.  It also has an RGB color sensor.
      * The light/distance sensor saturates at around 2" (5cm).  This means that targets that are 2"
      * or closer will display the same value for distance/light detected.
-     *
+     * <p>
      * Although you configure a single REV Robotics Color-Distance sensor in your configuration file,
      * you can treat the sensor as two separate sensors that share the same name in your op mode.
-     *
+     * <p>
      * In this example, we represent the detected color by a hue, saturation, and value color
      * model (see https://en.wikipedia.org/wiki/HSL_and_HSV).  We change the background
      * color of the screen to match the detected color.
-     *
+     * <p>
      * In this example, we  also use the distance sensor to display the distance
      * to the target object.  Note that the distance sensor saturates at around 2" (5 cm).
-     *
      */
     ColorSensor colorSensor;
     DistanceSensor distanceSensor;
+    ColorSensor colorSensor1;
+    DistanceSensor distanceSensor1;
 
     @Override
     public void runOpMode() {
@@ -56,12 +57,20 @@ public class REVColorSensor extends LinearOpMode {
         // get a reference to the distance sensor that shares the same name.
         distanceSensor = hardwareMap.get(DistanceSensor.class, "colorSensor");
 
+        // get a reference to the color sensor.
+        colorSensor1 = hardwareMap.get(ColorSensor.class, "colorSensor1");
+
+        // get a reference to the distance sensor that shares the same name.
+        distanceSensor1 = hardwareMap.get(DistanceSensor.class, "colorSensor1");
+
         // hsvValues is an array that will hold the hue, saturation, and value information.
         float hsvValues[] = {0F, 0F, 0F};
-
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues1[] = {0F, 0F, 0F};
         // values is a reference to the hsvValues array.
         final float values[] = hsvValues;
-
+        // values is a reference to the hsvValues array.
+        final float values1[] = hsvValues;
 
         // get a reference to the RelativeLayout so we can change the background
         // color of the Robot Controller app to match the hue detected by the RGB sensor.
@@ -75,9 +84,9 @@ public class REVColorSensor extends LinearOpMode {
         // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
         while (opModeIsActive()) {
             // convert the RGB values to HSV values.
-            Color.RGBToHSV( (colorSensor.red()),
-                     (colorSensor.green()),
-                     (colorSensor.blue()),
+            Color.RGBToHSV((colorSensor.red()),
+                    (colorSensor.green()),
+                    (colorSensor.blue()),
                     hsvValues);
 
             // send the info back to driver station using telemetry function.
@@ -89,23 +98,41 @@ public class REVColorSensor extends LinearOpMode {
             telemetry.addData("Blue ", colorSensor.blue());
             telemetry.addData("Hue", hsvValues[0]);
 
-            // change the background color to match the color detected by the RGB sensor.
-            // pass a reference to the hue, saturation, and value array as an argument
-            // to the HSVToColor method.
+            // loop and read the RGB and distance data.
+            // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV((colorSensor1.red()),
+                    (colorSensor1.green()),
+                    (colorSensor1.blue()),
+                    hsvValues1);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Distance (cm)",
+                    String.format(Locale.US, "%.02f", distanceSensor1.getDistance(DistanceUnit.CM)));
+            telemetry.addData("Alpha1", colorSensor1.alpha());
+            telemetry.addData("Red1  ", colorSensor1.red());
+            telemetry.addData("Green1", colorSensor1.green());
+            telemetry.addData("Blue1 ", colorSensor1.blue());
+            telemetry.addData("Hue1", hsvValues1[0]);
+
+                // change the background color to match the color detected by the RGB sensor.
+                // pass a reference to the hue, saturation, and value array as an argument
+                // to the HSVToColor method.
+                relativeLayout.post(new Runnable() {
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                    }
+                });
+
+                telemetry.update();
+
+            // Set the panel back to the default color
             relativeLayout.post(new Runnable() {
                 public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                    relativeLayout.setBackgroundColor(Color.WHITE);
                 }
             });
-
-            telemetry.update();
         }
-
-        // Set the panel back to the default color
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.WHITE);
-            }
-        });
     }
 }
